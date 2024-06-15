b1ddc0c628ad47d03b5166be2d014cf990f5fcac
AstaWorld <riteshchaubey195@gmail.com>
2024-06-15 22:34:02 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,5 +1,12 @@
-package demo;
+package com.demo;
 
 public class Demo {
+	public static void main(String[] args) {
+		
+		String a="Hello world";
+		Boolean val=a.contains("Hello");
+		System.out.println(val);
+		
+	}
 
 }
