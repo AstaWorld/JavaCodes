54962fddd2968aa7d616104191881b7655d150d8
AstaWorld <riteshchaubey195@gmail.com>
2024-06-15 23:33:41 +0530

    Add files via upload

---
+++ rite.java
@@ -0,0 +1,25 @@
+package com.rite;
+
+public class rite {
+  public static void main(String[] args){
+	  int age=20;
+	  
+	  
+	  if(age<18){
+	  System.out.println("you are underage");
+	  } else{
+		  System.out.println("you are valid");
+	  }
+	  System.out.println("continue..");
+	  
+	  
+	  int a=10;
+	  if(a%2==0) {
+	  System.out.println("Number is Even");
+  } else {
+	  System.out.println("Number is Odd");
+  }
+	  
+  }
+  
+}
