a79687231b36750e1603721eff159c14100db3e0
AstaWorld <riteshchaubey195@gmail.com>
2024-06-15 23:03:01 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -3,10 +3,18 @@ package com.demo;
 public class Demo {
 	public static void main(String[] args) {
 		
-		String a="Hello world";
-		Boolean val=a.contains("Hello");
-		System.out.println(val);
+		String a="Hello world"; //true 
+		 Boolean val=a.contains("Hello");
+         System.out.println(val);
 		
+		Boolean val1=a.equals("hello");//false
+		System.out.println(val1);
+		
+	    Boolean val3=a.equals("Hello world");// true
+		System.out.println(val3);
+		
+		Boolean val4=a.equalsIgnoreCase("hello world");
+		System.out.println(val4);
 	}
 
 }
