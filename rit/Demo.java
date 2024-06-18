44067d63eb704da935f745b074640b0a9ad0d55f
AstaWorld <riteshchaubey195@gmail.com>
2024-06-19 00:17:21 +0530

    Add files via upload

---
+++ rit/Demo.java
@@ -0,0 +1,75 @@
+package com.rit;
+import java.util.Scanner;
+public class Demo {
+	public static void main(String [] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		//question 4
+		/*System.out.println("please enter the number");
+		int a=sc.nextInt();
+		System.out.println("please enter the number");
+		int b=sc.nextInt();
+		
+		if(a>1&&b>1) {
+			System.out.println("the number is false");
+		}else if(a>0&&b>0) {
+			System.out.println("the number is false");
+		}else {
+			System.out.println("the number is true");
+		}*/
+		
+		
+		//question even or odd
+		/*System.out.println("please enter the number");
+		int a=sc.nextInt();
+		
+		if(a%2==0) {
+			System.out.println("the number is even");
+		}else if(a%1==0) {
+		}else {
+			System.out.println("the number is not defined");
+		}*/
+		
+		
+		
+		//question divisible by 5 and 11
+		
+		System.out.println("please enter the number");
+		int a=sc.nextInt();
+		
+		if (a%5==0) {
+			System.out.println("thenumber is divisible by 5");
+			
+		}else if(a%11==0) {
+			System.out.println("the number is divisible by 11");
+			
+		}else {
+			System.out.println("the number is normal");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+		
+		
+		
+		
+	}
+
+
