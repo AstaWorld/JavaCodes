9ad203b97fee58b3d5561921d40e588ba9c99c5c
AstaWorld <riteshchaubey195@gmail.com>
2024-06-24 00:48:33 +0530

    Add files via upload

---
+++ j/j.java
@@ -0,0 +1,31 @@
+package com.j;
+import java.util.Scanner;
+public class j {
+public static void main(String [] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("please enter the month");
+	int a=sc.nextInt();
+	System.out.println("please enter the year");
+	int b=sc.nextInt();
+	if(a==1||a==3||a==5||a==7||a==9||a==11) {
+		System.out.println("it consist of 31 days");
+	}else if (a==4||a==6||a==8||a==10||a==12) {
+		System.out.println("the number of days is 30 days");
+	}
+
+	if(b%4==0) {
+		System.out.println("the year is leap year");
+		if (a==2) {
+			System.out.println("the number of days is 29");
+		}
+		
+	}else {
+		System.out.println("this is not leap year");
+		if(a==2){
+			System.out.println("the no.of days is 28");
+		}
+	}
+	
+}	
+}	
+	
