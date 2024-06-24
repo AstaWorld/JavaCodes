1a160871dc4a82f3f382a8015648cff2fa2e3074
AstaWorld <riteshchaubey195@gmail.com>
2024-06-25 00:34:35 +0530

    Add files via upload

--- j/j.java
+++ j/j.java
@@ -2,29 +2,39 @@ package com.j;
 import java.util.Scanner;
 public class j {
 public static void main(String [] args) {
-	Scanner sc=new Scanner(System.in);
-	System.out.println("please enter the month");
-	int a=sc.nextInt();
-	System.out.println("please enter the year");
-	int b=sc.nextInt();
-	if(a==1||a==3||a==5||a==7||a==9||a==11) {
-		System.out.println("it consist of 31 days");
-	}else if (a==4||a==6||a==8||a==10||a==12) {
-		System.out.println("the number of days is 30 days");
-	}
-
-	if(b%4==0) {
-		System.out.println("the year is leap year");
-		if (a==2) {
-			System.out.println("the number of days is 29");
-		}
-		
-	}else {
-		System.out.println("this is not leap year");
-		if(a==2){
-			System.out.println("the no.of days is 28");
-		}
-	}
+Scanner sc=new Scanner(System.in);
+//	System.out.println("please enter the month");
+//	int a=sc.nextInt();
+//	System.out.println("please enter the year");
+//	int b=sc.nextInt();
+//	if(a==1||a==3||a==5||a==7||a==9||a==11) {
+//		System.out.println("it consist of 31 days");
+//	}else if (a==4||a==6||a==8||a==10||a==12) {
+//		System.out.println("the number of days is 30 days");
+//	}
+//
+//	if(b%4==0) {
+//		System.out.println("the year is leap year");
+//		if (a==2) {
+//			System.out.println("the number of days is 29");
+//		}
+//		
+//	}else {
+//		System.out.println("this is not leap year");
+//		if(a==2){
+//			System.out.println("the no.of days is 28");
+//		}
+//	}
+	
+System.out.println("please enter the alphabet");
+String g=sc.next();
+if(g=="a"||g=="e"||g=="i"||g=="o"||g=="u") {
+	System.out.println("the alphabet is vowel");
+}else {
+	System.out.println("the alphabet is consonant");
+}
+	
+	
 	
 }	
 }	
