764414df6efc65ab73bdb636da2b5257d05fa7d9
AstaWorld <riteshchaubey195@gmail.com>
2024-06-30 20:40:18 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,96 @@
+package com.a;
+import java.util.Scanner;
+public class A {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String choice="yes";
+	while(choice.equalsIgnoreCase("yes")) {
+	System.out.println("hello");
+	System.out.println("do you want to continue");
+	choice=sc.next();
+	}
+	
+	int x=sc.nextInt();
+	int y=sc.nextInt();
+	int sum=x+y;
+	System.out.println(sum);
+	System.out.println("do you want to continue");
+	
+	String hi= sc.next();
+	 while(choice.equalsIgnoreCase("yes")) { 
+		 int m=sc.nextInt();
+			int n=sc.nextInt();
+		 int sum1=m+n;
+		 System.out.println(sum1);
+	 System.out.println("do you want to continue");
+	hi=sc.next();
+
+
+}
+	
+}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
