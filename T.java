5b52bd5eadb0ab96aed2dc56ccb6b33714e6b92b
AstaWorld <riteshchaubey195@gmail.com>
2024-07-01 19:48:01 +0530

    Update T.java

--- T.java
+++ T.java
@@ -5,47 +5,47 @@ public class T {
 		//question calculator
 		Scanner sc= new Scanner(System.in);
 		
-//		String diffno=("yes");
-//		while (diffno.equalsIgnoreCase("yes")) 
-//		{
-//		System.out.println("please enter the number");
-//		int a=sc.nextInt();
-//		System.out.println("please enter the number");
-//		int b=sc.nextInt();
-//		String sameno=("yes");
-//		
-//		while(sameno.equalsIgnoreCase("yes")) {
-//		
-//		System.out.println( "please choice:- 1.add, 2.subtract,3.mul, 4.div, 5.mod");
-//		int choice=sc.nextInt();
-//		switch(choice) {
-//		case 1:
-//			System.out.println("add:-"+(a+b));
-//			break;
-//		case 2:
-//			System.out.println("subtract"+(a-b));
-//			break;	
-//	    case 3:
-//			System.out.println("mul"+(a*b));
-//		    break;	
-//	    case 4:
-//		    System.out.println("div"+(a/b));
-//		    break;	
-//        case 5:
-//			System.out.println("mod"+(a%b));
-//		    break;
-//		    default:
-//		    	System.out.println("invalid choice");
-//		}
-//		    	
-//		    	System.out.println("do you want to continue with same no.");
-//		    	 sameno=sc.next();}
-//		
-//	System.out.println("do you want to continue with diff  no");
-//	diffno=sc.next();
-//		
-//	}	
-//		//question  min&max
+		String diffno=("yes");
+		while (diffno.equalsIgnoreCase("yes")) 
+		{
+		System.out.println("please enter the number");
+		int a=sc.nextInt();
+		System.out.println("please enter the number");
+		int b=sc.nextInt();
+		String sameno=("yes");
+		
+		while(sameno.equalsIgnoreCase("yes")) {
+		
+		System.out.println( "please choice:- 1.add, 2.subtract,3.mul, 4.div, 5.mod");
+		int choice=sc.nextInt();
+		switch(choice) {
+		case 1:
+			System.out.println("add:-"+(a+b));
+			break;
+		case 2:
+			System.out.println("subtract"+(a-b));
+			break;	
+	    case 3:
+			System.out.println("mul"+(a*b));
+		    break;	
+	    case 4:
+		    System.out.println("div"+(a/b));
+		    break;	
+       case 5:
+			System.out.println("mod"+(a%b));
+		    break;
+		    default:
+		    	System.out.println("invalid choice");
+		}
+		    	
+		    	System.out.println("do you want to continue with same no.");
+		    	 sameno=sc.next();}
+		
+	System.out.println("do you want to continue with diff  no");
+	diffno=sc.next();
+		
+	}	
+		//question  min&max
 
 		int min=0;
 		int max=0;
@@ -69,60 +69,7 @@ public class T {
 			choice=sc.next();
 		}
 		System.out.println("max"+max);
-		System.out.println("min:"+min);
-	
-
-			
-			
-//int min = 0;
-//
-//int max = 0;
-//
-//String choice = ("yes");
-//
-//while (choice.equalsIgnoreCase("yes")){
-//
-//    
-//
-//    System.out.println("please enter number");
-//
-//    int a = sc.nextInt();
-//
-//    if (min == 0 && max == 0) {
-//
-//        min = a;
-//
-//        max = a;
-//
-//    }
-//
-//    
-//
-//    if (a < min) {
-//
-//        min = a;
-//
-//    }
-//
-//
-//
-//    if (a > max) {
-//
-//        max = a;
-//
-//    }
-//
-//    System.out.println("do you want to cont?");
-//
-//    choice = sc.next();
-//
-//}
-//
-//System.out.println("min:" + min);
-//
-//System.out.println("max:" + max);
-//		
-//			
+		System.out.println("min:"+min);			
 }			
 		
 			
