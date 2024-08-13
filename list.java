a351fb6a5a941f81b4703335fb0aa836df28da43
AstaWorld <riteshchaubey195@gmail.com>
2024-08-13 16:27:33 +0530

    Add files via upload

---
+++ list.java
@@ -0,0 +1,118 @@
+package com.t;
+//import java.util.Stack;  
+//import java.util.LinkedList;
+//import java.util.HashSet;
+//import java.util.LinkedHashSet;
+//import java.util.PriorityQueue;
+//import java.util.ArrayDeque;
+//import java.util.ArrayList;
+//import java.util.Vector;
+import java.util.TreeSet;
+public class list {
+public static void main(String [] args) {
+//	Stack<String> s=new Stack<>();
+//	s.push("a");
+//	s.push("b");
+//	s.push("c");
+//	s.push("d");
+//	s.push("e");
+//	
+//	s.pop();
+//	s.pop();
+//	
+//System.out.println(s.peek());
+////it shows the top element from bottom 
+//
+//
+//System.out.println(s.isEmpty());
+////it tells if the stack is empty or not
+	
+//	
+//	LinkedList<Integer> p= new LinkedList<>();
+//	p.add(23);
+//	p.add(24);
+//	p.add(1);
+//	p.add(2);
+//	p.add(3);
+//	
+//	System.out.println(p);
+//	
+	
+//	HashSet<Integer> p= new HashSet<>();
+//	p.add(3);
+//	p.add(4);
+//	p.add(5);
+//	p.add(6);
+//	p.add(5);
+//	p.add(6);
+	//HashSet does not print the duplicate element twice and print it only one time 
+	
+//	System.out.println(p);
+//	
+	
+//	LinkedHashSet<Integer> p= new LinkedHashSet<>();
+	// LinkedHashSet can add and remove the no. and can add 
+	
+//	p.add(6);
+//	p.add(4);
+//	p.add(24);
+//	p.add(23);
+//	p.add(26);
+//	p.add(27);
+//	
+//  p.remove(27);
+//System.out.println(p);
+	
+//  PriorityQueue<Integer> p= new PriorityQueue<>();
+//  p.add(23);  
+//  p.add(25); 
+//  p.add(1); 
+//  p.add(3); 
+//  p.add(5); 
+//  p.add(6); 
+//  
+//  p.remove();
+//  System.out.println(p);
+
+//	ArrayDeque<Integer> p=new ArrayDeque<>();
+//	p.add(23);
+//	p.add(21);
+//	p.add(22);
+//	p.add(24);
+//	p.add(26);
+//	p.add(56);
+//	
+//	p.removeFirst();
+//	p.removeLast();
+//	p.addFirst(20);
+//	p.addLast(12);
+//	
+//	System.out.println(p);
+	
+//	ArrayList<Integer> a=new ArrayList<>();
+//	a.add(23);
+//	a.add(25);  
+//	a.add(29);
+//	a.add(20);
+////	it has dynamic error
+//	System.out.println(a);
+	
+//	Vector<Integer> v=new Vector<>();
+//	v.add(24);
+//	v.add(25);
+//	v.add(26);
+//	v.add(27);
+//	
+//	//it performs in synchronized way
+//	
+//	System.out.println(v);
+	
+	TreeSet<Integer> T=new TreeSet<>();
+	T.add(100);
+	T.add(10);
+	T.add(1);
+	T.add(20);
+	
+	System.out.println(T);
+}
+}
