0be4fd9e5aa7dbc5f8a32cab3369f8c4263b44cb
Ritesh Chaubey <riteshchaubey195@gmail.com>
2025-11-12 15:42:29 +0530

    Delete OOps/Main.java

--- OOps/Main.java
+++
@@ -1,8 +0,0 @@
-package Inheritance;
-
-public class Main {
-	public static void main(String[]args) {
-		Bike bike=new Bike("moving", "on", 10, "on", 4, 10);
-		System.out.println(bike.run());
-	}
-}
